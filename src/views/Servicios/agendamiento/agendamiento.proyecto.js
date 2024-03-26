0b03e5440caa29d77ef18d59fad17c35b9418c34
EmanuelZapata09 <emanuelzpx@gmail.com>
2024-03-26 14:36:57 -0500

    Agendamiento

--- src/views/Servicios/agendamiento/agendamiento.proyecto.js
+++ src/views/Servicios/agendamiento/agendamiento.proyecto.js
@@ -1,22 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from 'axios'
-import { Calendar, dayjsLocalizer } from "react-big-calendar"
-import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import dayjs from 'dayjs'
 import "dayjs/locale/es";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { faUserGear, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { show_alerta } from 'src/fuctions.proyecto'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+
 
 dayjs.locale("es");
 
-const Agendamientos = () => {
-
+const Agendamiento = () => {
   const url = 'http://localhost:8081/api/agendamientos'
   const [agendamiento, setAgendamiento] = useState([])
-  const [SelectedDate, setSelectedDate] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [eventTitle, setEventTitle] = useState("");
+  const [selectEvent, setSelectEvent] = useState(null);
+  const [hora, setHora] = useState("");
+  const [id, setIdAgendamiento] = useState("");
+  const [operation, setOperation] = useState(1)
+  const [actualizacion, setActualizacion] = useState(false)
 
+  //Vehiculos
+  const [vehiculos, setVehiculos] = useState([])
+  const [placa, setPlaca] = useState('')
 
   useEffect(() => {
     getAgendamientos()
-  }, [])
+    getVehiculos()
+  }, [actualizacion ? agendamiento : null])
 
   const getAgendamientos = async () => {
     try {
@@ -27,25 +44,131 @@ const Agendamientos = () => {
     }
   }
 
+  const getVehiculos = async () => {
+    try {
+      const response = await axios.get('http://localhost:8081/api/vehiculo')
+      setVehiculos(response.data)
+    } catch (error) {
+      console.error('Error al obtener los vehiculos:', error.message)
+    }
+  }
+
+  const handleSelectSlot = (slotInfo) => {
+    setShowModal(true);
+    setSelectedDate(slotInfo.start);
+    setSelectEvent(null);
+  };
+  const handleSelectedEvent = (event) => {
+
+  };
+
+
+
+
   const localizer = dayjsLocalizer(dayjs)
-  
+
   const events = agendamiento.map(item => ({
-    start: dayjs(item.fecha).toDate(), 
-    end: dayjs(item.fecha).add(1, 'hour').toDate(), 
-    title: item.vehiculos_placa 
+    start: dayjs(item.fecha).toDate(),
+    end: dayjs(item.fecha).add(1, 'hour').toDate(),
+    title: item.vehiculos_placa
   }))
 
+  const validar = () => {
+    var parametros;
+    var metodo;
+
+
+    if (operation === 1) {
+      const fechaHora = selectedDate ? `${dayjs(selectedDate).format('YYYY-MM-DD')} ${hora}` : null;
+      if (!fechaHora) {
+        console.error('Por favor selecciona una fecha.');
+        return;
+      }
+
+      const eventoExistente = agendamiento.find(item => dayjs(item.fecha).format('YYYY-MM-DD HH:mm') === fechaHora);
+      if (eventoExistente) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Ya hay un agendamiento registrado para la misma hora en este día.',
+        });
+        return;
+      }
+
+
+      parametros = { idAgendamiento: id, fecha: fechaHora, vehiculos_placa: placa };
+
+      console.log(parametros)
+      metodo = 'POST';
+    } else if (operation === 2) {
+      parametros = {};
+      metodo = 'PUT';
+
+      console.log(parametros)
+    }
+    enviarSolicitud(metodo, parametros);
+  }
+
+  const enviarSolicitud = async (metodo, parametros) => {
+    await axios({ method: metodo, url: url, data: parametros }).then(function (respuesta) {
+      var tipo = respuesta.data[0];
+      if (metodo === 'POST') {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Agendamiento agregado con exito",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        //document.getElementById('btn-close').click();
+      }
+      //  else if (metodo === 'PUT') {
+      //   Swal.fire({
+      //     position: "center",
+      //     icon: "success",
+      //     title: "Empleado editado con exito",
+      //     showConfirmButton: false,
+      //     timer: 1500
+      //   });
+      //   document.getElementById('btnCerrar').click();
+      // }
+      // if (metodo === 'DELETE') {
+      //   Swal.fire({
+      //     position: "center",
+      //     icon: "success",
+      //     title: "Empleado eliminado con exito",
+      //     showConfirmButton: false,
+      //     timer: 1500
+      //   });
+      //   document.getElementById('btnCerrar').click();
+      // }
+
+      setActualizacion(true)
+
+      if (tipo === 'success') {
+        //document.getElementById('btnCerrar').click();
+        // getRoles();
+      }
+    })
+      .catch(function (error) {
+        show_alerta('Error en la solicitud', 'error');
+        console.log(error);
+      });
+  }
+
+
+
   return (
-    <div style={{
-      height: "95vh",
-      width: "70vw"
-    }}>
+    <div style={{ height: "500px" }}>
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
-        titleAccessor="title"
+        style={{ margin: "50px" }}
+        selectable={true}
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleSelectedEvent}
         messages={{
           allDay: 'Todo el día',
           previous: 'Anterior',
@@ -60,10 +183,165 @@ const Agendamientos = () => {
           event: 'Evento',
           showMore: total => `+ Ver más (${total})`
         }}
-    
       />
-    </div>
-  )
-}
 
-export default Agendamientos
+      {showModal && (
+        <div
+          className="modal"
+          style={{
+            display: "block",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">
+                  Agregar Agendamiento
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => {
+                    setShowModal(false);
+                    setEventTitle("");
+                    setSelectEvent(null);
+                  }}
+                ></button>
+              </div>
+              <div className="modal-body">
+
+                <div className='input-group mb-3'>
+                  <span className='input-group-text'><FontAwesomeIcon icon={faUserGear} /></span>
+                  <input
+                    type='text'
+                    id='id'
+                    className='form-control'
+                    placeholder='ID'
+                    value={id}
+                    onChange={(e) => setIdAgendamiento(e.target.value)}
+
+                  />
+                </div>
+
+                <div className='input-group mb-3'>
+                  <span className='input-group-text'><FontAwesomeIcon icon={faUserGear} /></span>
+                  <select id='placa' className='form-select' value={placa} onChange={(e) => setPlaca(e.target.value)} style={{ marginRight: '12px' }}>
+                    <option value='' disabled>Vehiculo</option>
+                    {vehiculos.map((v) => (
+                      <option key={v.placa} value={v.placa}>{v.placa}</option>
+                    ))}
+                  </select>
+                  <button className='botones-azules' data-bs-toggle='modal' data-bs-target='#modalEmpleados'  >
+                    <FontAwesomeIcon icon={faPlusCircle} /> Añadir
+                  </button>
+                </div>
+
+                <div className='input-group mb-3'>
+                  <span className='input-group-text'><FontAwesomeIcon icon={faUserGear} /></span>
+                  <input
+                    type='time'
+                    id='horaInicio'
+                    className='form-control'
+                    placeholder='Hora'
+                    value={hora}
+                    onChange={(e) => setHora(e.target.value)}
+                  />
+                </div>
+
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="botones-azules"
+                  onClick={() => validar()}
+                >
+                  Guardar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div id='modalVehiculoEditar' className='modal fade' aria-hidden='true' >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">
+                {selectEvent ? "Editar Agendamiento" : "Agregar Agendamiento"}
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={() => {
+                  setShowModal(false);
+                  setEventTitle("");
+                  setSelectEvent(null);
+                }}
+              ></button>
+            </div>
+            <div className="modal-body">
+
+              <div className='input-group mb-3'>
+                <span className='input-group-text'><FontAwesomeIcon icon={faUserGear} /></span>
+                <input
+                  type='text'
+                  id='id'
+                  className='form-control'
+                  placeholder='ID'
+                  value={id}
+                  onChange={(e) => setIdAgendamiento(e.target.value)}
+
+                />
+              </div>
+
+              <div className='input-group mb-3'>
+                <span className='input-group-text'><FontAwesomeIcon icon={faUserGear} /></span>
+                <select id='placa' className='form-select' value={placa} onChange={(e) => setPlaca(e.target.value)} style={{ marginRight: '12px' }}>
+                  <option value='' disabled>Vehiculo</option>
+                  {vehiculos.map((v) => (
+                    <option key={v.placa} value={v.placa}>{v.placa}</option>
+                  ))}
+                </select>
+                <button className='botones-azules' data-bs-toggle='modal' data-bs-target='#modalEmpleados'  >
+                  <FontAwesomeIcon icon={faPlusCircle} /> Añadir
+                </button>
+              </div>
+
+              <div className='input-group mb-3'>
+                <span className='input-group-text'><FontAwesomeIcon icon={faUserGear} /></span>
+                <input
+                  type='time'
+                  id='horaInicio'
+                  className='form-control'
+                  placeholder='Hora'
+                  value={hora}
+                  onChange={(e) => setHora(e.target.value)}
+                />
+              </div>
+
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="botones-azules"
+                onClick={() => validar()}
+              >
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Agendamiento;
