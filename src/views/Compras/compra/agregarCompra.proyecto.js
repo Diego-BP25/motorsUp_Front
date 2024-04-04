af807345a9fde57bf8849d842c44d6e87818ca8a
Diegoapl17 <perezlopezdiego037@gmail.com>
2024-04-04 08:15:15 -0500

    compras 70%

--- src/views/Compras/compra/agregarCompra.proyecto.js
+++ src/views/Compras/compra/agregarCompra.proyecto.js
@@ -3,13 +3,33 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash, faPlusCircle, faFloppyDisk, faCalendar, faToggleOff, faIdCardClip, faTag, faFileText, faTruckField, faHashtag, faBagShopping, faDollar } from '@fortawesome/free-solid-svg-icons'
+import {
+    CAvatar,
+    CButton,
+    CButtonGroup,
+    CCard,
+    CCardBody,
+    CCardFooter,
+    CCardHeader,
+    CCol,
+    CProgress,
+    CRow,
+    CTable,
+    CTableBody,
+    CTableDataCell,
+    CTableHead,
+    CTableHeaderCell,
+    CTableRow,
+} from '@coreui/react'
+
+import WidgetsDropdown from 'src/views/widgets/WidgetsDropdown.proyecto'
 
 const AgregarCompra = () => {
     // API URL
     const url = 'http://localhost:8081/api/compras';
 
     // Estados del formulario
-    const [idCompra, setIdCompra] = useState('');
+    const [total, setTotal] = useState('');
     const [descripcion, setDescripcion] = useState('');
     const [estado, setEstado] = useState('');
     const [fechaCompra, setFechaCompra] = useState('');
@@ -17,7 +37,6 @@ const AgregarCompra = () => {
     const [proveedorSeleccionado, setProveedorSeleccionado] = useState('');
     const [productos, setProductos] = useState([]);
     const [idProducto, setIdProducto] = useState('');
-    const [productoSeleccionado, setProductoSeleccionado] = useState('');
     const [cantidad, setCantidad] = useState('');
     const [precio, setPrecio] = useState('');
     const [productosCompra, setProductosCompra] = useState([]);
@@ -28,6 +47,10 @@ const AgregarCompra = () => {
         getProveedores();
         getProductos();
     }, []);
+
+    useEffect(() => {
+        calcularTotal(productosCompra);
+    }, [productosCompra]);
 
     // Obtener lista de proveedores
     const getProveedores = async () => {
@@ -80,6 +103,7 @@ const AgregarCompra = () => {
             setCantidad('');
             setPrecio('');
             setIdProducto('');
+            calcularTotal([...productosCompra, { idProducto, cantidad, precio, subtotal }]);
         } else {
             Swal.fire({
                 icon: 'error',
@@ -87,20 +111,25 @@ const AgregarCompra = () => {
             });
         }
     };
+    const calcularTotal = (productos) => {
+        const totalCompra = productos.reduce((acc, producto) => acc + producto.subtotal, 0);
+        setTotal(totalCompra);
+    };
 
 
 
     // Función para guardar la compra
     const guardarCompra = async () => {
         try {
-
+            const subtotalProductos = productosCompra.reduce((acc, producto) => acc + producto.subtotal, 0);
             const nuevaCompra = {
                 idCompra: consecutivo,
                 descripcionCompra: descripcion,
                 estadoCompra: estado,
                 fechaCompra: fechaCompra,
                 proveedores_idProveedor: proveedorSeleccionado,
-                detalleProductos: productosCompra,
+                total: subtotalProductos,
+                detalleProductos: productosCompra
             };
             console.log(nuevaCompra)
             await axios.post(url, nuevaCompra);
@@ -116,6 +145,7 @@ const AgregarCompra = () => {
             setFechaCompra('');
             setProveedorSeleccionado('');
             setProductosCompra([]);
+            setTotal(subtotalProductos.toString());
         } catch (error) {
             console.error('Error al guardar la compra:', error.message);
             Swal.fire({
@@ -128,14 +158,14 @@ const AgregarCompra = () => {
     return (
         <div className='App' >
             <div className='container mt-5' >
-                <div style={{ marginRight: 'auto', marginTop: '-5%', }}>
-                    <h3>Agregar compra</h3>
-                </div>
-                <form onSubmit={guardarCompra} >
-                    <div className="container">
-                        <div className="row">
-                            <div className="col" >
-                                <div className='input-group mb-3' style={{ border: '1px solid', maxWidth: '55%', paddingBottom: '1%', paddingLeft: '2%', paddingTop: '2%', paddingRight: '2%', marginLeft: '-2%' }}>
+                <div className='row'>
+                    <div style={{ marginRight: 'auto', marginTop: '-5%', }}>
+                        <h3>Agregar compra</h3>
+                    </div>
+                    <div className='col-md-6' >
+                        <form onSubmit={guardarCompra} className="flex-grow-1 mr-3" >
+                            <div className="container">
+                                <div className='input-group mb-3' id='container1' style={{ maxWidth: '55%', padding: '3.5%', marginTop: '-1%', marginLeft: '-2%' }}>
 
                                     <div className='input-group mb-3' onChange={obtenerIdConsecutivo()}>
                                         <label htmlFor='idCompra' className='input-group-text'><FontAwesomeIcon icon={faIdCardClip} /></label>
@@ -154,7 +184,7 @@ const AgregarCompra = () => {
 
                                     <div className='input-group mb-3' >
                                         <label htmlFor='fechaCompra' className='input-group-text'><FontAwesomeIcon icon={faCalendar} /></label>
-                                        <input id='fechaCompra' className="form-control" value={fechaCompra} onChange={(e) => setFechaCompra(e.target.value)} />
+                                        <input type='datetime-local' id='fechaCompra' className="form-control" value={fechaCompra} onChange={(e) => setFechaCompra(e.target.value)} />
                                     </div>
 
                                     <div className='input-group mb-3' >
@@ -166,6 +196,7 @@ const AgregarCompra = () => {
                                             ))}
                                         </select>
                                     </div>
+
                                     <h4 style={{ marginRight: 'auto', }}>Productos</h4>
 
                                     <div className='input-group mb-3' >
@@ -195,60 +226,58 @@ const AgregarCompra = () => {
                                             <FontAwesomeIcon icon={faFloppyDisk} /> Agregar producto
                                         </button>
                                     </div>
-
                                 </div>
                             </div>
-
-                            <div className="col">
-
-                                <div style={{ border: '1px solid', maxWidth: '135%', maxHeight: '50%', marginLeft: '-40%', padding: '3%', overflow: 'scroll'}}>
-                                    <h4>Productos agregados</h4>
-                                    <table className='table'>
-                                        <thead  style={{ position: 'sticky', top: 0, backgroundColor: 'white' }} >
-                                            <tr >
-                                                <th>ID Producto</th>
-                                                <th>Cantidad</th>
-                                                <th>Precio</th>
-                                                <th>Subtotal</th>
-                                                <th>Acciones</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody >
-                                            {productosCompra.map((producto, index) => (
-                                                <tr key={index} >
-                                                    <td>{producto.idProducto}</td>
-                                                    <td>{producto.cantidad}</td>
-                                                    <td>{producto.precio}</td>
-                                                    <td>{producto.subtotal}</td>
-                                                    <td>
-                                                        <button type='button' onClick={() => eliminarProducto(index)} className='btn btn-danger'>
-                                                            <FontAwesomeIcon icon={faTrash} />
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
+                            <div key={"buttonGuardar"} className='input-group mb-3' style={{ marginTop: '-13%', marginLeft: '166.7%' }}>
+                                <button type='submit' className='botones-azules' style={{ width: '60%' }}>
+                                    <FontAwesomeIcon icon={faFloppyDisk} /> Guardar compra
+                                </button>
                             </div>
+
+                            <div className='input-group mb-3' style={{ marginTop: '-8.5%', marginLeft: '65.8%', maxHeight: '35px', marginBottom: '35px' }}>
+                                <label htmlFor='total' className='input-group-text'><FontAwesomeIcon icon={faDollar} /></label>
+                                <input type='number' className="form-control" id='total' value={total}/>
+                            </div>
+                        </form>
+
+                    </div>
+
+                    <div className='col-md-6' >
+
+                        <div id='container1' style={{ maxWidth: '135%', maxHeight: '70%', marginLeft: '-40%', padding: '3%', overflow: 'scroll' }}>
+                            <h4>Productos agregados</h4>
+                            <table className='table'>
+                                <thead style={{ position: 'sticky', top: 0, backgroundColor: 'white' }} >
+                                    <tr >
+                                        <th>ID Producto</th>
+                                        <th>Cantidad</th>
+                                        <th>Precio</th>
+                                        <th>Subtotal</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody >
+                                    {productosCompra.map((producto, index) => (
+                                        <tr key={index} >
+                                            <td>{producto.idProducto}</td>
+                                            <td>{producto.cantidad}</td>
+                                            <td>{producto.precio}</td>
+                                            <td>{producto.subtotal}</td>
+                                            <td>
+                                                <button type='button' onClick={() => eliminarProducto(index)} className='btn btn-danger'>
+                                                    <FontAwesomeIcon icon={faTrash} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-
-                    <div className='input-group mb-3' style={{ marginTop: '-9.5%', marginLeft: '32%' }}>
-                        <label htmlFor='precio' className='input-group-text'><FontAwesomeIcon icon={faDollar} /></label>
-                        <input type='number' className="form-control" id='precio' value={precio} onChange={(e) => setPrecio(e.target.value)} />
-                    </div>
-
-                    <div key={"buttonGuardar"} className='col-md-4 offset-md-5' style={{ marginTop: '-5.5%', marginLeft: '76.7%' }}>
-                        <button type='submit' className='botones-azules' style={{ width: '60%', marginTop: '3%' }}>
-                            <FontAwesomeIcon icon={faFloppyDisk} /> Guardar compra
-                        </button>
-                    </div>
-
-                </form>
+                </div>
             </div>
 
-        </div>
+        </div >
     );
 };
 
--- src/views/funcionesExtras.proyecto.js
+++ src/views/funcionesExtras.proyecto.js
@@ -18,3 +18,14 @@ export const actualizarCamposConsultas = (asignarEstados, estados, nombreCampos)
             asignarEstados[i](`${estados[nombreCampos[i]]}`);
     }
 }
+
+export const fecha2 = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  };
